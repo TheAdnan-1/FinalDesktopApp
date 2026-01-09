f04913cf9e3c0c0b9e3d61f3cdae7522162ae366
Mahabubul Alam <alam2207001@stud.kuet.ac.bd>
2026-01-09 19:33:18 +0600

    Register Controller made successfully

--- src/main/java/org/example/theadnan/RegisterController.java
+++ src/main/java/org/example/theadnan/RegisterController.java
@@ -1,4 +1,32 @@
 package org.example.theadnan;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import org.example.theadnan.services.AuthService;
+
 public class RegisterController {
+
+    @FXML private TextField name, email, age, profession, hobby;
+    @FXML private PasswordField password;
+    @FXML private Label status;
+
+    public void registerUser() {
+        try {
+            boolean success = AuthService.register(
+                    email.getText(),
+                    name.getText(),
+                    Integer.parseInt(age.getText()),
+                    profession.getText(),
+                    hobby.getText(),
+                    password.getText()
+            );
+
+            status.setText(success
+                    ? "Registration successful!"
+                    : "Email already exists!");
+
+        } catch (Exception e) {
+            status.setText("Invalid input!");
+        }
+    }
 }
